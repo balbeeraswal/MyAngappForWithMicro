f88b0b645b70a1f2049c79c8d35cd65c48edf249
Balbeer Aswal <balbeeraswal@gmail.com>
2026-05-19 22:06:55 +0530

    url changed

--- src/app/EmployeeService.ts
+++ src/app/EmployeeService.ts
@@ -9,7 +9,7 @@ import { Employee,ApiResponse } from './models/employee.model';
 export class EmployeeService   {
 
   private apiUrlGetEmps="https://employeewebapp-bheseyhvhuawejg5.centralindia-01.azurewebsites.net/api/Employee/GetEmployees";
-  private apiUrlPostEmp="https://employeewebapp-bheseyhvhuawejg5.centralindia-01.azurewebsites.net/api/Employee";
+  private apiUrlPostEmp="https://employeewebapp-bheseyhvhuawejg5.centralindia-01.azurewebsites.net/api/Employee/AddEmployee";
   private apiUrl="https://localhost:7283/api/Employee/GetEmployees";
    private getEmpByIdapiUrl="https://localhost:7283/api/Employee";
   private postapiUrl="https://localhost:7283/api/Employee";
